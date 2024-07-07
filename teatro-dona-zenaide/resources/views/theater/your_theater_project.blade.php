b8b33a4e05d3d41d90499b490e40571e1a7c5fca
GiovaniWHB <upessoa30@gmail.com>
2024-07-07 13:48:59 -0300

    Convenção de Nomes das Views em Laravel
    Alterando o nome da view de Sobre Nós e de Informações da Peça para entrar na convenção de nomes do Laravel para views de nome composto.
    
    Criação da view de Seu Projeto No Teatro.

---
+++ teatro-dona-zenaide/resources/views/theater/your_theater_project.blade.php
@@ -0,0 +1,12 @@
+{{-- Puxando o layout --}}
+@extends('layout')
+
+{{-- Mudando o título da página dinamicamente --}}
+@section('view-title', 'Seu Projeto No Teatro - Teatro Dona Zenaide')
+
+{{-- Conteúdo da página --}}
+@section('content')
+
+    
+
+@endsection
