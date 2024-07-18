20dc9b17cd7e4f1e6cc718786516864dc4a5c53e
GiovaniWHB <upessoa30@gmail.com>
2024-07-18 00:50:27 -0300

    Dividindo o CSS em Arquivos Separados
    Dividindo o CSS com todas os estilos de todas as telas em arquivos separados para cada tela, e um arquivo global contendo variáveis e estilos do Layout.
    
    Adicionado os novos arquivos CSS no app.js para serem carregados no Layout pelo Vite.

--- teatro-dona-zenaide/resources/js/app.js
+++ teatro-dona-zenaide/resources/js/app.js
@@ -3,7 +3,12 @@ import './bootstrap';
 import './scripts';
 
 // Imports CSS
-import '../css/style.css';
+import '../css/global.css';
+
+import '../css/home.css';
+import '../css/play_info.css';
+import '../css/about_us.css';
+import '../css/your_theater_project.css';
 
 // Vite Assets Img
 import.meta.glob([
