cff5293a3cb7d4209831a58133834b6b9d438f5b
GiovaniWHB <upessoa30@gmail.com>
2024-07-19 02:37:15 -0300

    Responsividade da Seção de Hero da Tela Home
    Criação da responsividade da seção de Hero da tela Home.

--- teatro-dona-zenaide/resources/views/theater/home.blade.php
+++ teatro-dona-zenaide/resources/views/theater/home.blade.php
@@ -15,12 +15,12 @@
                 {{-- Conteúdo do Hero --}}
                 <div class="hero-content col-md-12">
                     <div class="hero-text">
-                        <h1 class="tnr-bold tnr-title-size tnr-title-size--xlg">TEATRO DONA ZENAIDE</h1>
+                        <h1 class="tnr-bold tnr-title-size tnr-title-size--xlg">TEATRO DONA <br>ZENAIDE</h1>
                         <p class="roboto-regular">Entre no palco da imaginação e deixe-se envolver pelas histórias que ganham vida sob os holofotes. Bem-vindo ao nosso universo teatral, onde cada cena é uma jornada única e emocionante. Descubra conosco a magia das artes cênicas e embarque em uma experiência que transcende o tempo e o espaço. O espetáculo está prestes a começar. Você está pronto para ser parte dessa história?</p>
                     </div>
 
                     {{-- Botão de Ver Peças --}}
-                    <a class="main-btn main-btn--hero" href="#">
+                    <a class="main-btn main-btn--hero" href="#cards-section">
                         <span>VER PEÇAS</span>
                         <span class="ri--arrow-down-s-line"></span>
                     </a>
