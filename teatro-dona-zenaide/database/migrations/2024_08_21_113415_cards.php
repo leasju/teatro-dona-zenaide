91f580636233a08e713f067394b12c86e87b3e07
leasju <jusouzaleandro@gmail.com>
2024-09-05 10:47:23 -0300

    Adição dos campos opcionais da migration 'Cards'

--- teatro-dona-zenaide/database/migrations/2024_08_21_113415_cards.php
+++ teatro-dona-zenaide/database/migrations/2024_08_21_113415_cards.php
@@ -17,12 +17,21 @@ return new class extends Migration
 
             //OBRIGATÓRIO -------------------------------------------------
             $table->text('nomeEsp'); //Nome do espetáculo
-            $table->text('tempEsp'); //Temporada
+            $table->text('tempEsp'); //Temporada do espetáculo
             $table->date('dataEsp'); // Data do espetáculo
-            $table->time('horaEsp'); // Horário do espetáculo
             $table->text('duracaoEsp'); // Duração do espetáculo
             $table->text('tempoEsp'); // Tempo do espetáculo (minutos)
-            $table->text('classifEsp'); // Classificação indicatia
+            $table->text('classifEsp'); // Classificação indicativa
+            $table->text('descEsp', 255); // Descrição do espetáculo
+            $table->text('roteiristaEsp', 255); // Roteirista do espetáculo
+            $table->text('elencoEsp', 255); // Elenco do espetáculo
+            $table->text('direcaoEsp', 255); // Responsável pela direção 
+            $table->text('figurinoEsp', 255); // Responsável pelo figurino 
+            $table->text('cenoEsp', 255); // Cenografia (Responsável pelo cenário)
+            $table->text('luzEsp', 255); // Responsável pela iluminação
+            $table->text('sonoEsp', 255); // Responsável pela sonorização
+            $table->text('produçãoEsp', 255); // Produção do espetáculo
+            $table->string('urlCompra'); // URL de compra de ingresso para o espetáculo 
 
             //Imagens
             $table->text('imgCard'); // Imagem do Card 
@@ -32,28 +41,21 @@ return new class extends Migration
             $table->text('imgCar_4'); // Imagem 4 - Carrossel 
             $table->text('imgCar_5'); // Imagem 5 - Carrossel 
 
-            $table->text('sinoEsp', 255); // Sinopse do espetáculo
-            $table->text('roteiristaEsp', 255); // Roteirista 
-            $table->text('elencoEsp', 255); // Elenco do espetáculo
-            $table->text('direcaoEsp', 255); // Direção do espetáculo
-            $table->text('figurinoEsp', 255); // Figurino do espetáculo
-            $table->text('cenoEsp', 255); // Cenografia
-            $table->text('luzEsp', 255); // Iluminação
-            $table->text('sonoEsp', 255); // Responsável sonorização
-            $table->text('produçãoEsp', 255); // Produção do espetáculo
-
-
+            /*Horários
+            $table->time('horaEsp_1'); // Horário 1
+            $table->time('horaEsp_2'); // Horário 2
+            $table->time('horaEsp_3'); // Horário 3
+            $table->time('horaEsp_4'); // Horário 4
+            $table->time('horaEsp_5'); // Horário 5
+            */
 
             //OPCIONAL ---------------------------------------------------
-           
-          
-           
+            $table->text('costEsp', 255); // Costureira do espetáculo
+            $table->text('cenoAssistEsp', 255); // Assistente de cenografia
+            $table->text('cenoTec', 255); // Cenotécnico do espetáculo
+            $table->text('coProdução', 255); // Co-produção do espetáculo
+            $table->text('agradecimentos', 255); // Agradecimentos
 
-            $table->text('producaoEsp', 255); // Figurino do espetáculo
-
-            
-            
-            
 
             $table->timestamps(); 
         });
